df1448aa971458eb1d23677b4e1b40461573cba2
Christian Assing <chris@ca-net.org>
2016-12-12 12:10:07 +0100

    fix node.MangerStatus.Leader for worker nodes

--- app.py
+++ app.py
@@ -38,6 +38,8 @@ def index():
     nodes = []
     for node in client.nodes():
         node['tasks'] = sorted(tasks[node['ID']], key=lambda x: x["service_name"])
+        if "ManagerStatus" not in node:
+            node['Manager'] = {'Leader': False}
         nodes.append(node)
 
     return render_template('index.html', nodes=nodes, reload=int(request.args.get('reload', 100)) * MINUTES)
